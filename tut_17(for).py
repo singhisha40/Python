173a2f074764516018c165d0927d9d83bd3e4e5d
singhisha40 <141261626+singhisha40@users.noreply.github.com>
2023-12-24 18:52:57 +0530

    Add files via upload

---
+++ tut_17(for).py
@@ -0,0 +1,20 @@
+#for loop
+'''
+import time
+
+name = "Isha"
+
+for i in name:
+    print(i)
+    if(i == 'h'):
+        print("here h is present")
+'''
+
+# for k in range(5):
+#     print(k + 1)
+
+# for j in range (136, 201):
+    # print(j)
+
+# for i in range(1, 15, 3):
+    # print(i)
---
+++ tut_18(while).py
@@ -0,0 +1,13 @@
+#while loop
+
+# count = 5
+# while(count > 0):
+#     print(count)
+#     count = count - 1
+
+a = float(input("Enter a num\n"))
+while(a > 0):
+    print(a)
+    a = a - 0.5
+
+    
